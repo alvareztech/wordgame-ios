013d6f2023e2ffaa245bca49c3292d5790806827
Daniel Alvarez <daniel@alvarez.tech>
2022-11-13 11:22:08 -0800

    Initial Commit

---
+++ WordGame/WordGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGameApp.swift
+//  WordGame
+//
+//  Created by Daniel Alvarez on 13/11/22.
+//
+
+import SwiftUI
+
+@main
+struct WordGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
